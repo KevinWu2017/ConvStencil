f577090215620901185825bd0cce2e426a6802b1
cpwu <cpwu2017@126.com>
2025-04-10 21:37:51 +0800

    Use 1 for missing dimension

--- src/1d/main.cu
+++ src/1d/main.cu
@@ -172,7 +172,7 @@ int main(int argc, char *argv[])
     // print brief info
 
     // printf("INFO: shape = %s,  n = %d, times = %d\n", ShapeStr[compute_shape], n, times);
-    std::cout << "ConvStencil, " << ShapeStr[compute_shape] << ", , , " << n << ", " << times << ", ";
+    std::cout << "ConvStencil, " << ShapeStr[compute_shape] << ", 1, 1, " << n << ", " << times << ", ";
 
     int cols = n + 2 * HALO; //+1
 
--- src/2d/main.cu
+++ src/2d/main.cu
@@ -304,7 +304,7 @@ int main(int argc, char *argv[])
 
     // print brief info
     // printf("INFO: shape = %s, m = %d, n = %d, times = %d\n", ShapeStr[compute_shape], m, n, times);
-    std::cout << "ConvStencil, " << ShapeStr[compute_shape] << ", , " << m << ", " << n << ", " << times << ", ";
+    std::cout << "ConvStencil, " << ShapeStr[compute_shape] << ", 1, " << m << ", " << n << ", " << times << ", ";
 
     int rows = m + 2 * halo;
     int cols = n + 2 * halo + 2;
